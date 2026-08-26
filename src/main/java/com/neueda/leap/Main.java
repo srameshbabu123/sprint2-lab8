3ec5b7f8ab55d1de788494b075ab5d3a916177d8
Sharath Kumar Rameshbabu <sharathrameshbabu5@gmail.com>
2026-08-26 17:35:57 +0000

    new branch and has its commit

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,7 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello world from the team's Sprint 1 project skeleton");
+        System.out.println("Hello world from the team's Sprint 2 project skeleton");
         System.out.println("Container is up. Sleeping so you can inspect it.");
         Thread.sleep(600_000);
     }
